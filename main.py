d33497a61f049b161a8b5c7942b76055b4a4d6b9
JaviFuentes <javierfuentesalonso@gmail.com>
2017-11-13 16:47:40 +0100

    General structure. Loading images are buggy

--- main.py
+++ main.py
@@ -18,21 +18,48 @@ sys.path.insert(0, abs_file_path)
 
 import SegNet as sn
 import utils
+import training_ops
+from Data_utils import gray_to_RGB
 
 sess = tf.InteractiveSession()
 
-images = tf.placeholder("float", [None, 224, 224, 3])
-img1 = utils.load_image("./Data/images/0001TP_006690.png")
-batch = img1.reshape((1, 224, 224, 3))
+images_ph = tf.placeholder(tf.float32, [None, 224, 224, 3])
+imgIn = utils.load_image(".\\Data\\images\\0001TP_006720.png")
+imgIn = imgIn.reshape((1, 224, 224, 3))
 
-segnet = sn.SegNet(num_class = 10)
-segnet.build(images)
+labels_ph= tf.placeholder(tf.int32, [None, 224, 224])
+imgLabel = utils.load_image(".\\Data\\labels\\Gray_labels2\\0001TP_006720.png")
+imgLabel = imgLabel.reshape((1, 224, 224))
+#gray_to_RGB(skimage.img_as_int(imgLabel),"Label.png")
 
+segnet = sn.SegNet(num_class = 12)
+segnet.build(images_ph)
+
+predictions = segnet.convD5_2
+loss_op = training_ops.calc_loss(predictions, labels_ph)
+train_op = training_ops.train_network(loss_op)
+
+
+#init = tf.initialize_all_variables()
+init =  tf.global_variables_initializer()
 with tf.Session(config=tf.ConfigProto(gpu_options=(tf.GPUOptions(per_process_gpu_memory_fraction=0.5)))) as sess:
 #with tf.device('/cpu:0'):
 #    with tf.Session() as sess:
     #images = tf.placeholder("float", [2, 224, 224, 3])
-    #writer = tf.summary.FileWriter('./Tensorboard', sess.graph) #Saves the graph in the Tensorboard folder
-    feed_dict = {images: batch}
-    result = sess.run(segnet.argmax, feed_dict=feed_dict)
-    print(result)
+    
+    sess.run(init)
+    for i in range(100):
+        feed_dict = {images_ph: imgIn, labels_ph: imgLabel}
+        fetches_train = [train_op, loss_op]
+        #writer = tf.summary.FileWriter('./Tensorboard', sess.graph) #Saves the graph in the Tensorboard folder
+    
+        res = sess.run(fetches = fetches_train, feed_dict=feed_dict)
+    
+        #print("Train WTF "+res[0])
+        print(res[1])
+
+        feed_test = {images_ph: imgIn}
+        img = sess.run(segnet.argmax, feed_dict=feed_test)
+
+        gray_to_RGB(img[0])
+        
--- training_ops.py
+++ training_ops.py
@@ -5,33 +5,35 @@
 import tensorflow as tf
 
 # 1) Define cross entropy loss
-def calc_loss(predictions, labels)
-    # computing cross entropy per sample, use softmax_cross_entropy_with_logits to avoid problems with log(0)
-   	#cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels, predictions)
-   	#I believe using that one the labels will have to be in [widthxheight] shape
-   	#instead of 3d [widthxheightxclassnum]
-    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=predictions)
-   	# Sum over all pixels
-	cross_entropy = tf.reduce_sum(cross_entropy, [1, 2])
-   	# Average over samples
-   	# Averaging makes the loss invariant to batch size, which is very nice.
-   	cross_entropy = tf.reduce_mean(cross_entropy)
-   	return cross_entropy
+def calc_loss(predictions, labels):
+    """computing cross entropy per sample, use softmax_cross_entropy_with_logits to avoid problems with log(0)
+   	    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels, predictions)
+   	    I believe using that one the labels will have to be in [widthxheight] shape
+   	    instead of 3d [widthxheightxclassnum]"""
+    with tf.variable_scope("Loss"):
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=predictions)
+   	    # Sum over all pixels
+        cross_entropy = tf.reduce_sum(cross_entropy, [1, 2])
+   	    # Average over samples
+   	    # Averaging makes the loss invariant to batch size, which is very nice.
+        cross_entropy = tf.reduce_mean(cross_entropy)
+        return cross_entropy
 
 # 2) Define accuracy
-def calc_accuracy(predictions, labels)
-	#to be implemented
-	return accuracy
+def calc_accuracy(predictions, labels):
+	with tf.variable_scope("Accuracy"):    
+        #to be implemented
+	    return accuracy
 
 # 3) Define the training op
-def train_network(loss)
-	# Defining our optimizer
-	optimizer = tf.train.MomentumOptimizer(learning_rate=0.1, momentum=0.9)
-    #optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
+def train_network(loss):
+    with tf.variable_scope("TrainOp"):
+        # Defining our optimizer
+        #optimizer = tf.train.MomentumOptimizer(learning_rate=0.1, momentum=0.9)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001)
+        # Computing our gradients
+        grads_and_vars = optimizer.compute_gradients(loss)
 
-    # Computing our gradients
-	grads_and_vars = optimizer.compute_gradients(loss)
-
-    # Applying the gradients
-	train_op = optimizer.apply_gradients(grads_and_vars)
-	return train_op
+        # Applying the gradients
+        train_op = optimizer.apply_gradients(grads_and_vars)
+        return train_op
