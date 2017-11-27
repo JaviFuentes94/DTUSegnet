19040bae34e8d9f412b0ec1d6b9f7f7889088f97
Harold <harold@macbook-pro-5.eduroam.wireless.dtu.dk>
2017-11-27 15:07:26 +0100

    add batch class

---
+++ batch.py
@@ -0,0 +1,36 @@
+import glob
+import random
+import utils
+import numpy as np
+
+class batch:
+    def __init__(self):
+        images_filenames = glob.glob('./Data/images/*.png')
+        labels_filenames = glob.glob('./Data/labels/*.png')
+        self.val_size = 100
+        rand_idx =  list(range(0,len(images_filenames)))
+        random.shuffle(rand_idx)
+        self.train_images_filenames = [images_filenames[i] for i in rand_idx[self.val_size+1:]]
+        self.train_labels_filenames = [labels_filenames[i] for i in rand_idx[self.val_size+1:]]
+        self.val_images_filenames = [images_filenames[i] for i in rand_idx[:self.val_size]]
+        self.val_labels_filenames = [labels_filenames[i] for i in rand_idx[:self.val_size]]
+        self.train_rand_idx = list(range(0,len(self.train_images_filenames)))
+
+    def get_train(self,size):
+        b_im = np.zeros((size, 224, 224, 3))
+        b_lab = np.zeros((size, 224, 224))
+        random.shuffle(self.train_rand_idx)
+        for i in range(size):
+            idx = self.train_rand_idx[i]
+            b_im[i] = utils.load_image_input(self.train_images_filenames[idx])
+            b_lab[i] = utils.load_image_labels(self.train_labels_filenames[idx])
+        return b_im, b_lab
+
+    def get_validation(self):
+
+        v_im = np.zeros((self.val_size, 224, 224, 3))
+        v_lab = np.zeros((self.val_size, 224, 224))
+        for idx in range(self.val_size):
+            v_im[idx] = utils.load_image_input(self.val_images_filenames[idx])
+            v_lab[idx] = utils.load_image_labels(self.val_labels_filenames[idx])
+        return v_im, v_lab
